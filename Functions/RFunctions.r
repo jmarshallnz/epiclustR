183105826eeb8584dce9888436a1ae9e2bd85240
Jonathan Marshall <jmarshall@never.you.mind>
2016-09-15 11:37:51 +1200

    Optimise the R update to use current expected cases. Idea is we can save that between runs.

--- Functions/RFunctions.r
+++ Functions/RFunctions.r
@@ -168,9 +168,12 @@ RLikelihoodRUX2 <- function(j,k,curr,prop,state) {
   U   <- state$U
   X   <- state$X
   betaX <- state$betaX
-  num <- dpois(cases[j:k,],rep(n,each=k-j+1)*exp(fe+rep(prop,mbs)+rep(U,each=k-j+1)+X[j:k,mbrg]*rep(betaX[mbrg],each=k-j+1)), log=TRUE)
-  den <- dpois(cases[j:k,],rep(n,each=k-j+1)*exp(fe+rep(curr,mbs)+rep(U,each=k-j+1)+X[j:k,mbrg]*rep(betaX[mbrg],each=k-j+1)), log=TRUE)
-  sum(num - den)
+  # lambda here is the current rate
+  lambda_curr <- rep(n,each=k-j+1)*exp(fe+rep(curr,mbs)+rep(U,each=k-j+1)+X[j:k,mbrg]*rep(betaX[mbrg],each=k-j+1))
+  lambda_prop <- lambda_curr * rep(exp(prop-curr),mbs)
+# sum(dpois(cases[j:k,],lambda_prop, log=TRUE)-
+#     dpois(cases[j:k,],lambda_curr, log=TRUE))
+  sum(cases[j:k,] * rep(prop - curr,mbs) - lambda_prop + lambda_curr)
 }
 
 RLikelihoodRUX3 <- function(j,k,curr,prop) {
