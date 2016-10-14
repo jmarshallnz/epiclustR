c1a1a7f519adffabfbb14bc34c104c0ef9d62532
Jonathan Marshall <jmarshall@xbmc.org>
2016-10-15 11:06:32 +1300

    Removes igraph dependency from the five colouring algorithm

--- pkg/R/map_helpers.R
+++ pkg/R/map_helpers.R
@@ -1,64 +1,63 @@
-#' @importFrom igraph %>% neighbors degree delete_vertices add_vertices add_edges graph_from_adjacency_matrix
-NULL
 
-find_degree_five <- function(g) {
-  wch = which(igraph::degree(g) == 5)
-  for (i in seq_along(wch)) {
-    nb = igraph::neighbors(g, wch[i])
-    # find 2 neighbours N1, N2 such that they are not neighbours of each other, and such that each has
-    # degree at most 7, and such that they're not neighbours of each other
-    nb = nb[igraph::degree(g, nb) <= 7]
-    if (length(nb) >= 2) {
-      # run through and find the neighbours of each pair
-      l = lapply(seq_along(nb), function(x) { setdiff(nb[-x], igraph::neighbors(g, nb[x])) })
-      nb_wch = which(unlist(lapply(l, function(x) { length(x) > 0 })))
-      if (length(nb_wch) > 0) {
-        return(list(x = wch[i], n1 = nb[nb_wch[1]], n2 = V(g)[l[[nb_wch[1]]][1]]))
+five_colour_graph <- function(A) {
+  if (!(is.matrix(A) && is.logical(A))) {
+    stop("Not an adjacency matrix\n")
+  }
+
+  # helper function to find the vertex of degree 5 with neighbours
+  # of degree no more than 7 that are not adjacent, which must
+  # exist for a planar graph
+  find_degree_five <- function(A) {
+    deg = rowSums(A)
+    wch = which(deg == 5)
+    for (i in seq_along(wch)) {
+      nb = which(A[wch[i],])
+      # find 2 neighbours N1, N2 such that they are not neighbours of each other, and such that each has
+      # degree at most 7, and such that they're not neighbours of each other
+      nb = nb[deg[nb] <= 7]
+      if (length(nb) >= 2) {
+        # run through and find the neighbours of each pair. There is likely a more
+        # efficient way to do this!
+        l = lapply(seq_along(nb), function(x) { setdiff(nb[-x], which(A[nb[x],])) })
+        nb_wch = which(unlist(lapply(l, function(x) { length(x) > 0 })))
+        if (length(nb_wch) > 0) {
+          return(c(wch[i], nb[nb_wch[1]], l[[ nb_wch[1] ]][1]))
+        }
       }
     }
+    stop("Can't find a 5 degree vertex with non-adjacent neighbours of degree no more than 7. Non planer graph?\n")
   }
-  return(NULL)
-}
 
-five_colour_graph <- function(g) {
-  d = igraph::degree(g)
-  cols = numeric(length(d))
+  deg = rowSums(A)
+  cols = numeric(length(deg))
 
-  if (length(d) == 1) {
+  if (length(deg) == 1) {
     # empty graph - we're done, use any colour we like
     cols[1] = 1
-  } else if (any(d < 5)) {
+  } else if (any(deg < 5)) {
     # remove the vertex, and colour the previous
-    v = which(d < 5)[1]
-    cols[-v] = five_colour_graph(igraph::delete_vertices(g, v))
-    nb_cols = cols[igraph::neighbors(g, v)]
-    col = setdiff(1:5, nb_cols)
+    v = which(deg < 5)[1]
+    cols[-v] = five_colour_graph(A[-v,-v,drop=FALSE])
+    col = setdiff(1:5, cols[A[v,]])
     if (length(col) == 0) {
       stop("Error colouring graph\n")
     }
     cols[v] = col[1]
-  } else if (any (d == 5)) {
-    xn = find_degree_five(g)
-    if (is.null(xn)) {
-      stop("Can't find a 5 degree vertex with non-adjacent neighbours of degree no more than 7. Non planer graph?\n")
-    }
-    # now we remove node x, n1, n2 from the graph, and add a new node N1/N2 that
-    # links previous N1,N2 to the others
-    linkto = union(igraph::neighbors(g, xn$n1), igraph::neighbors(g, xn$n2))
-    el = rep(length(V(g))+1, 2*length(linkto))
-    el[seq(1,length(el), by=2)] = linkto
-    g_del = igraph::add_vertices(g, 1) %>%
-      igraph::add_edges(el) %>%
-      igraph::delete_vertices(c(xn$x, xn$n1, xn$n2))
-    col_sub = five_colour_graph(g_del)
-    cols[-c(xn$x, xn$n1, xn$n2)] <- col_sub[-length(col_sub)]
-    cols[c(xn$n1, xn$n2)] <- col_sub[length(col_sub)]
-    nb_cols = cols[igraph::neighbors(g, xn$x)]
-    col = setdiff(1:5, nb_cols)
+  } else if (any (deg == 5)) {
+    v = find_degree_five(A)
+    # add new vertex N1/N2 linking to their neighbours
+    linkto = A[v[1],] | A[v[2],]
+    A_new = rbind(cbind(A, linkto), c(linkto, FALSE))
+
+    # remove old vertices x, n1, n2 and colour
+    col_sub = five_colour_graph(A_new[-v, -v, drop=FALSE])
+    cols[-v] <- col_sub[-length(col_sub)]
+    cols[v[2:3]] <- col_sub[length(col_sub)]
+    col = setdiff(1:5, cols[A[v[1],]])
     if (length(col) == 0) {
       stop("Error colouring graph\n")
     }
-    cols[xn$x] = col[1]
+    cols[v[1]] = col[1]
   } else {
     stop("Error colouring graph - not a planer graph?\n")
   }
@@ -68,13 +67,13 @@ five_colour_graph <- function(g) {
 build_region_adjacency <- function(mbrg, nb) {
   # build the LUT
   rgmb = init_region_lut(mbrg)
-  adj = matrix(0, length(rgmb), length(rgmb))
+  adj = matrix(FALSE, length(rgmb), length(rgmb))
   for (i in 1:length(rgmb)) {
     spat_in  = rgmb[[i]]
     spat_all = unique(as.numeric(nb[spat_in,-1]))
     spat_nb  = setdiff(spat_all, spat_in)
     rg_nb = unique(mbrg[spat_nb])
-    adj[i, rg_nb] = 1
+    adj[i, rg_nb] = TRUE
   }
   # check:
   if (!isTRUE(all.equal(adj, t(adj)))) {
@@ -93,10 +92,8 @@ build_region_adjacency <- function(mbrg, nb) {
 five_colour_map <- function(mbrg, nb) {
   # build the adjacency matrix
   a <- build_region_adjacency(mbrg, nb)
-  # convert to a graph
-  g <- igraph::graph_from_adjacency_matrix(a, mode='undirected')
   # colour the graph
-  col_index = five_colour_graph(g)
+  col_index = five_colour_graph(a)
   # return the coloured regions
   col_index[mbrg]
 }
