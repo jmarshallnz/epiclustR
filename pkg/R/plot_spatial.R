0d88cb861bcc821bd0d0f416c2f2d07efe376808
Jonathan Marshall <jmarshall@never.you.mind>
2016-10-17 09:33:20 +1300

    adds plotting/tabling routines

---
+++ pkg/R/plot_spatial.R
@@ -0,0 +1,72 @@
+#' @importFrom maptools readShapeSpatial
+#' @importFrom sp bbox plot
+#' @importFrom dplyr %>% select mutate filter
+#' @importFrom grDevices col2rgb rgb
+NULL
+
+#' Plot spatial trends from an epiclustR model
+#' 
+#' @param mod the model to plot
+#' @param data the data used to fit the model
+#' @param shapefile the shape file to use for the map
+#' @param period which time periods(s) to plot. Default to NULL (all times)
+#' @param threshold threshold at which to show outbreaks. Any outbreak with probability over this threshold will
+#' be highlighted. Defaults to 0.2
+#' @param bbox The bounding box to use for the map, useful for zooming in on portions. Should be a matrix. Default to NULL
+#' which uses the bounding box of the shapefile.
+#' @export
+plot_spatial <- function(mod, data, shapefile, period=NULL, threshold=0.2, bbox=NULL) {
+  map <- maptools::readShapeSpatial(shapefile)
+
+  spat_risk <- apply(ssapply(mod, extract_variable, 'U'), 1, mean)
+
+  # grab out the outbreak data
+  X <- ssapply(mod, extract_variable, 'X')
+  mX = apply(X, 1:2, mean)
+
+  # filter down the period
+  if (!is.null(period)) {
+    # TODO: make this work with dates
+    mX = mX[period, ,drop=FALSE]
+  }
+
+  # filter down the threshold
+  outbreaks <- data.frame(Region = 1:ncol(mX), P = apply(mX, 2, max))
+  outbreaks <- outbreaks[outbreaks$P >= threshold,,drop=FALSE]
+
+  # form the data.frame
+  spat_risk <- cbind(data$spat_list, Risk=spat_risk)
+
+  map_dat <- map@data %>%
+    dplyr::left_join(spat_risk, by=c('MB06' = 'Spatial')) %>%
+    dplyr::left_join(outbreaks, by='Region')
+
+  # figure out some break-points for colours
+  up_bks = quantile(map_dat$Risk[map_dat$Risk > 0], seq(1,9,by=2)/9)
+  lo_bks = quantile(map_dat$Risk[map_dat$Risk < 0], seq(8,0,by=-2)/9)
+  bks = round(pmax(up_bks, lo_bks), 2)
+
+  # define the colours
+  alpha <- function(col, x = 0.5) {
+    rgb(t(col2rgb(col)), alpha = x * 255, maxColorValue = 255)
+  }
+  brewerBrBG9 <- c("#01665E","#35978F","#80CDC1","#C7EAE5","#F5F5F5","#F6E8C3","#DFC27D","#BF812D","#8C510A")
+  cols <- alpha(brewerBrBG9, 0.7)
+  vals <- cut(map_dat$Risk, breaks = c(-bks, bks))
+  map_col <- cols[vals]
+
+  if (is.null(bbox) || !is.matrix(bbox))
+    bbox = sp::bbox(map)
+
+  # plot the map
+  plot(map, col=map_col, lwd=0.02, border='grey80', xlim=bbox[1,], ylim=bbox[2,])
+
+  # overlay the outbreaks
+  red <- function(x) {
+    rgb(1, 0, 0, ifelse(is.na(x), 0, x))
+  }
+  plot(map, add=TRUE, col=red(map_dat$P), border=NA)
+  #  xy <- t(simplify2array(lapply(map@polygons, function(x) { x@labpt })))
+  #  text(xy[!is.na(map_dat$P),1], xy[!is.na(map_dat$P),2], map_dat$Region[!is.na(map_dat$P)])
+  legend('topright', legend=c(levels(vals), 'Outbreak'), fill = c(cols, red(1)), cex=0.6)
+}
---
+++ pkg/R/tables.R
@@ -0,0 +1,40 @@
+#' Table of outbreaks from an epiclustR model
+#' 
+#' @param mod The model to table
+#' @param data The data used for the model
+#' @param period The time period to use. Defaults to NULL which is all time
+#' @param threshold The threshold for an outbreak. Only outbreaks that exceed this threshold will be displayed. Defaults to 0.2
+#' @export
+table_outbreaks <- function(mod, data, period = NULL, threshold = 0.2) {
+  # pull out the outbreaks
+  X <- ssapply(mod, extract_variable, 'X')
+  mX = apply(X, 1:2, mean)
+  mX = data.frame(mX, check.names = FALSE)
+  mX$ReportWeek = as.Date(row.names(data$cases))
+  regions = tidyr::gather_(mX, key_col='Region', value_col='P', gather_cols=setdiff(names(mX), 'ReportWeek'), convert=TRUE)
+
+  # match these up with cases
+  case_outbreaks <- data$case_list %>% dplyr::left_join(data$spat_list, by="Spatial") %>%
+    dplyr::left_join(regions)
+  case_outbreaks <- case_outbreaks[,c('CaseID', 'ReportWeek', 'Region', 'P')]
+
+  # filter out the date period
+  if (!is.null(period)) {
+    # TODO: Period should be date-ized
+    case_outbreaks <- case_outbreaks[case_outbreaks$ReportWeek %in% period]
+  }
+
+  # filter out based on the threshold
+  case_outbreaks <- case_outbreaks[case_outbreaks$P >= threshold,]
+
+  # pull out the cases that correspond to these outbreaks
+  tab <- case_outbreaks %>%
+    dplyr::arrange(ReportWeek) %>%
+    dplyr::mutate(Dupes = duplicated(cbind(Region, ReportWeek, P))) %>%
+    dplyr::mutate(Region = ifelse(!Dupes, Region, ''),
+                  ReportWeek = ifelse(!Dupes, as.character(ReportWeek), ''),
+                  P = ifelse(!Dupes, P, '')) %>%
+    dplyr::select(-Dupes)
+
+  tab
+}
