08b436586a13ba1291a8a881cc8c7fb401eb57e0
Jonathan Marshall <jmarshall@never.you.mind>
2016-10-17 09:31:25 +1300

    Tidy up mcmc.r for the new model fitting layout

--- mcmc.r
+++ mcmc.r
@@ -4,61 +4,41 @@ library(tidyr)
 
 # the priors and MCMC control parameters
 prior <- init_priors()
+
 control <- init_control(thinning=50, samples = 1000, burnin=20)
 
 spat <- read.table("Meshblocks.txt", header=TRUE)
 
 nb   <- load_spatial_neighbours("Weights.GAL")
 popn <- spat$Population
-mbrg <- spat$CAU
+mbrg <- spat$Zone7
 
-case <- read.csv("CaseData/cases.csv")
+case <- read.csv("CaseData/cases.csv", stringsAsFactors = FALSE)
 
-case <- case %>% filter(Meshblock06 %in% spat$Meshblock06)
+case <- case %>% filter(Meshblock06 %in% spat$Meshblock06) %>%
+  mutate(ReportWeek = as.Date(ReportWeek)) %>%
+  filter(Year >= 2006 & Year <= 2015)
 
 # now compute the number of cases per meshblock per time
-cases <- matrix(0, max(case$WeekFromStart), length(mbrg))
-rownames(cases) <- 1:max(case$WeekFromStart)
+weeks <- seq(min(case$ReportWeek), max(case$ReportWeek), by = 7)
+cases <- matrix(0, length(weeks), length(mbrg))
+rownames(cases) <- as.character(weeks)
 colnames(cases) <- spat$Meshblock06
 
-tab <- case %>% group_by(WeekFromStart, Meshblock06) %>% summarize(Cases = n())
+tab <- case %>% group_by(ReportWeek, Meshblock06) %>% summarize(Cases = n())
 week_by_mb <- tab %>% spread(Meshblock06, Cases, fill = 0)
-cases[as.character(week_by_mb$WeekFromStart),names(week_by_mb)[-1]] <- as.matrix(week_by_mb[,-1])
+cases[as.character(week_by_mb$ReportWeek),names(week_by_mb)[-1]] <- as.matrix(week_by_mb[,-1])
 
 # assemble data
 data <- check_data(list(cases=cases, popn=popn, mbrg=mbrg, nb=nb))
+data$spat_list <- spat %>% select(ID = ID, Spatial = Meshblock06, Region = Zone7)
+data$case_list <- case %>% select(CaseID = EpiSurvNumber, Spatial = Meshblock06, ReportWeek)
 
 # fit the model
 print(system.time({
-posterior <- fit_model(data, prior, control, seed = 1)
+mod <- fit_model(data, prior, control, seed = 1)
 }))
 
-# do analysis
-ssapply <- function(x, fun, ...) {
-  simplify2array(lapply(x, function(xx, fun, ...) { simplify2array(lapply(xx, fun, ...)) }, fun = fun, ...))
-}
-
-extract_variable <- function(x, variable) {
-  x[[variable]]
-}
-
-# now dump out our posterior chains
-R = ssapply(posterior, extract_variable, 'R')
-U = ssapply(posterior, extract_variable, 'U')
-betaX = ssapply(posterior, extract_variable, 'betaX')
-fe = ssapply(posterior, extract_variable, 'fe')
-kR = ssapply(posterior, extract_variable, 'kR')
-kU = ssapply(posterior, extract_variable, 'kU')
-X = ssapply(posterior, extract_variable, 'X')
-pX = ssapply(posterior, extract_variable, 'pX')
-ecases = ssapply(posterior, cases_per_time, data=data, smoothed=FALSE)
-scases = ssapply(posterior, cases_per_time, data=data, smoothed=TRUE)
-plot(apply(ecases, 1, median), type='l')
-lines(apply(scases, 1, median), col='red')
-#hist(apply(R, 1, ess))
-
-library(ggplot2)
-pX %>% data.frame %>%
-  mutate(iteration=seq_len(n())) %>%
-  gather('chain', 'value', -iteration) %>%
-  ggplot(aes(x=iteration, y=value)) + geom_line(aes(colour=chain))
+# save model
+zone7 = list(data, mod)
+save(list = c('zone7'), file='zone7.RData')
