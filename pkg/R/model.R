53db36b53fc893adad9ac451043ef5af08deb3eb
Jonathan Marshall <jmarshall@never.you.mind>
2016-10-26 15:45:25 +1300

    Allow an empty region map

--- pkg/R/model.R
+++ pkg/R/model.R
@@ -131,6 +131,8 @@ fit_chain <- function(chain, data, prior, control) {
 #' @export
 check_data <- function(data) {
   # initialize the region lookup table
+  if (is.null(data$mbrg))
+    data$mbrg <- rep(1, ncol(data$cases))
   data$rgmb <- init_region_lut(data$mbrg)
 
   # check we have population where we have cases
